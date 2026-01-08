d69e7267a2df3df880a8ea7507fcdcb38b95474a
pedrocrespo13 <pedrocrespo5pt23sm@gmail.com>
2026-01-08 11:39:18 +0000

    small fixes

--- myTasks/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'myTasks',
-  webDir: 'www'
-};
-
-export default config;
